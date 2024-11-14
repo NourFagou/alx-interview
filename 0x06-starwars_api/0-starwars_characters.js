aec63e320a289e6c81de98f011ec3081935c0adc
NourFagou <nourfagou@gmail.com>
2024-11-14 12:38:05 +0000

    mandatory

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,28 @@
+#!/usr/bin/node
+
+const request = require('request');
+const movieId = process.argv[2];
+const apiUrl = `https://swapi-api.hbtn.io/api/films/${movieId}/`;
+
+// Make a request to get the movie details
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error(error);
+    return;
+  }
+
+  const movie = JSON.parse(body);
+  const characters = movie.characters;
+
+  // For each character URL, make a request to get the character's name
+  characters.forEach((characterUrl) => {
+    request(characterUrl, (error, response, body) => {
+      if (error) {
+        console.error(error);
+        return;
+      }
+      const character = JSON.parse(body);
+      console.log(character.name);
+    });
+  });
+});
